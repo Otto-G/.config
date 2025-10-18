6dca1abbd80429d8b264e2099cdb4f36265cefb6
Michael <z28blackpearl@gmail.com>
2025-10-17 23:10:59 -0400

    Add oh-my-zsh config

--- setupSystem.sh
+++ setupSystem.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+# Link vimrc to home directory
 export VIMINIT='source ~/.config/vimrc'
 export MYVIMRC='~/.config/vimrc'
 
+# Link .zshrc to home directory
 ln -s $HOME/.config/.zshrc $HOME/.zshrc
+
+# Install oh-my-zsh
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
